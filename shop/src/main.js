6b7b2ff4525fb39b78837e5f4928dc3800d4fdaf
XiaTerry <1363314380@qq.com>
2018-12-23 22:44:10 +0800

    login & register

--- shop/src/main.js
+++ shop/src/main.js
@@ -6,10 +6,12 @@ import './assets/js/rem.js'
 import './assets/css/reset.scss'
 //引入vant组件库
 
-import {Tabbar, TabbarItem,NavBar,Icon, Lazyload} from 'vant';
+import {Tabbar, TabbarItem,NavBar,Icon, Lazyload,Tab,Tabs
+,Field,Cell, CellGroup,Button} from 'vant';
 import 'vant/lib/index.css';
 
-Vue.use(Tabbar).use(TabbarItem).use(NavBar).use(Icon).use(Lazyload);
+Vue.use(Tabbar).use(TabbarItem).use(NavBar).use(Icon).use(Lazyload).use(
+  Tab).use(Tabs).use(Field).use(Cell).use(CellGroup).use(Button);
 Vue.config.productionTip = false
 
 new Vue({
--- shop/src/views/user/User.vue
+++ shop/src/views/user/User.vue
@@ -1,12 +1,75 @@
 <template>
     <div>
-        我的
+        <van-tabs>
+            <van-tab title="登录">
+               <van-cell-group>
+                <van-field
+                    v-model="loginUsername"
+                    required
+                    clearable
+                    label="用户名"
+                    placeholder="请输入用户名"
+                />
+                <van-field
+                    v-model="loginPassword"
+                    clearable
+                    type="password"
+                    label="密码"
+                    placeholder="请输入密码"
+                    required
+                />
+                </van-cell-group>
+                <van-button size="large" type="primary">登录</van-button>
+            </van-tab>
+            <van-tab title="注册">
+               <van-cell-group>
+                <van-field
+                    v-model="registerUsername"
+                    required
+                    clearable
+                    label="用户名"
+                    placeholder="请输入用户名"
+                />
+                <van-field
+                    v-model="registerPassword"
+                    clearable
+                    type="password"
+                    label="密码"
+                    placeholder="请输入密码"
+                    required
+                />
+                </van-cell-group>
+                <van-button size="large" type="primary">注册</van-button>
+            </van-tab>
+        </van-tabs>
+        
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    
+   data() {
+       return {
+           loginUsername: '',
+           loginPassword: '',
+           registerUsername: '',
+           registerPassword: ''
+       }
+   },
+   methods: {
+       //注册
+       register() {
+           axios.post({
+               method: 'post',
+               url: '/user',
+               data: {
+                   username: this.registerUsername,
+                   password: this.registerPassword
+               }
+           })
+       }
+   },
 }
 </script>
 
